21bc011057f78006cb56023acbb60d72b1a69db4
niebl <caro.niebl@uni-muenster.de>
2026-09-09 20:46:26 +0200

    move templates into dedicated directory

---
+++ templates/invoice_text.sh
@@ -0,0 +1,21 @@
+SALUTATION_DE="Guten Tag,"
+SALUTATION_EN="Greetings,"
+
+INVOICEBODY_DE="Für die erbrachten Arbeiten erhalten Sie hiermit die folgende Rechnung.
+\\\\Bitte Überweisen Sie den fälligen Betrag auf das folgende Bankkonto:
+\\begin{center}
+\\accountBankName, \\textbf{\\accountIBAN} (BIC: \\accountBIC) \\\\
+\\end{center}
+Bitte geben Sie auch die Rechnungsnummer (\\invoiceReference) im Betreff an."
+INVOICEBODY_EN="For the listed services you are receiving the following invoice.
+\\\\Please transfer the due amount to the following bank account:
+\\begin{center}
+\\accountBankName, \\textbf{\\accountIBAN} (BIC: \\accountBIC) \\\\
+\\end{center}
+Please mention the invoice number (\\invoiceReference) in the subject line."
+
+USTGNOTE_DE="Die Rechnung enthält gemäß § 19 UStG keine Umsatzsteuer.\\\\"
+USTGNOTE_EN="Die Rechnung enthält gemäß § 19 UStG keine Umsatzsteuer.\\\\VAT-Free according to § 19 section 1 UStG."
+
+CLOSING_DE="Mit freundlichen Grüßen,"
+CLOSING_EN="Best,"
